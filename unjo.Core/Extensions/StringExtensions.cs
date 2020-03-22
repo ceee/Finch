676f218c3f26b2d5830b4a44a7961784bde699d2
Tobias Klika <cee@live.at>
2020-03-22 14:29:12 +0100

    ++

--- unjo.Core/Extensions/StringExtensions.cs
+++ unjo.Core/Extensions/StringExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 
 namespace unjo.Core.Extensions
 {
@@ -13,19 +11,66 @@ namespace unjo.Core.Extensions
       return toStartWith + input.TrimStart(toStartWith);
     }
 
+
     public static string EnsureStartsWith(this string input, char value)
     {
       return input.StartsWith(value.ToString(CultureInfo.InvariantCulture)) ? input : value + input;
     }
+
 
     public static string EnsureEndsWith(this string input, char value)
     {
       return input.EndsWith(value.ToString(CultureInfo.InvariantCulture)) ? input : input + value;
     }
 
+
     public static string EnsureEndsWith(this string input, string toEndWith)
     {
       return input.EndsWith(toEndWith.ToString(CultureInfo.InvariantCulture)) ? input : input + toEndWith;
+    }
+
+
+    public static string TrimEnd(this string value, string forRemoving)
+    {
+      if (String.IsNullOrEmpty(value)) return value;
+      if (String.IsNullOrEmpty(forRemoving)) return value;
+
+      while (value.EndsWith(forRemoving, StringComparison.InvariantCultureIgnoreCase))
+      {
+        value = value.Remove(value.LastIndexOf(forRemoving, StringComparison.InvariantCultureIgnoreCase));
+      }
+      return value;
+    }
+
+
+    public static string TrimStart(this string value, string forRemoving)
+    {
+      if (String.IsNullOrEmpty(value)) return value;
+      if (String.IsNullOrEmpty(forRemoving)) return value;
+
+      while (value.StartsWith(forRemoving, StringComparison.InvariantCultureIgnoreCase))
+      {
+        value = value.Substring(forRemoving.Length);
+      }
+      return value;
+    }
+
+
+    public static bool HasValue(this string input)
+    {
+      return !String.IsNullOrWhiteSpace(input);
+    }
+
+
+    public static bool IsNullOrEmpty(this string input)
+    {
+      return String.IsNullOrEmpty(input);
+    }
+
+
+    public static bool IsNullOrWhiteSpace(this string input)
+    {
+      return String.IsNullOrWhiteSpace(input);
     }
   }
 }
--- unjo.Web/Program.cs
+++ unjo.Web/Program.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
 
 namespace unjo.Web
 {
@@ -13,14 +11,33 @@ namespace unjo.Web
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Process ID: " + Process.GetCurrentProcess().Id);
       CreateHostBuilder(args).Build().Run();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+      return Host
+        .CreateDefaultBuilder(args)
+        .ConfigureLogging((context, builder) =>
+        {
+          IConfigurationSection configuration = context.Configuration.GetSection("Logging");
+          builder.ClearProviders();
+
+          if (context.HostingEnvironment.IsDevelopment())
+          {
+            builder.AddConsole();
+          }
+
+          if (configuration.GetValue("AsFile", false))
+          {
+            builder.AddFile(configuration);
+          }
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup>();
+        });
+    }
   }
 }
