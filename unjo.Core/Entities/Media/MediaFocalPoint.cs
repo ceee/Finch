5dfbb9a7c9edff981d9a20a06b0ef0f0638c3f70
Tobias Klika <cee@live.at>
2020-03-23 19:14:46 +0100

    add media entities

---
+++ unjo.Core/Entities/Media/MediaFocalPoint.cs
@@ -0,0 +1,12 @@
+﻿namespace unjo.Core.Entities
+{
+  /// <summary>
+  /// The focal point sets the point of interest in an image with x/y coordinates from 0-1
+  /// </summary>
+  public class MediaFocalPoint
+  {
+    public decimal Left { get; set; }
+
+    public decimal Top { get; set; }
+  }
+}
