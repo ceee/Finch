69637f48fc9d8eb516e4bfb839b84cd52b233f92
Tobias Klika <cee@live.at>
2020-04-05 00:54:18 +0200

    Create SectionController.cs

---
+++ zero.Web/Controllers/SectionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using zero.Core;
+
+namespace zero.Web.Controllers
+{
+  [AllowAnonymous]
+  public class SectionController : BackofficeController
+  {
+    private ZeroOptions Options { get; set; }
+
+    public SectionController(IZeroConfiguration config, IOptionsMonitor<ZeroOptions> options) : base(config)
+    {
+      Options = options.CurrentValue;
+    }
+
+
+    public IActionResult GetAll()
+    {
+      return Json(Options.Sections.ToList());
+    }
+  }
+}
