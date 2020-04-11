3b5892f70c63b6a73656b34bf021618ea565151c
Tobias Klika <cee@live.at>
2020-04-11 13:34:15 +0200

    pagination works with dropdown

--- zero.Web/App/Components/pagination.vue
+++ zero.Web/App/Components/pagination.vue
@@ -1,17 +1,16 @@
 ﻿<template>
   <div class="ui-pagination" v-if="pages > 1">
-    <ui-icon-button class="ui-pagination-next" type="white" title="Previous" icon="fth-chevron-left" :disabled="page < 2" @click="set(page - 1)" />
-    <ui-dropdown align="bottom">
-      <template v-slot:button>
-        <button type="button" class="ui-button type-blank caret-down ui-pagination-select">
-          <span class="ui-button-text" v-localize="{ key: '@ui.page_xofy', tokens: { x: page, y: pages }}"></span>
-          <i class="ui-button-caret fth-chevron-down"></i>
-        </button>
-      </template>
-      <div>page select</div>
-      <!--<ui-dropdown-list />-->
-    </ui-dropdown>
-    <ui-icon-button class="ui-pagination-next" type="white" title="Next" icon="fth-chevron-right" :disabled="page >= pages" @click="set(page + 1)" />
+    <ui-icon-button class="ui-pagination-next" type="white" title="@ui.pagination.previous" icon="fth-chevron-left" :disabled="page < 2" @click="set(page - 1)" />
+    <div class="ui-pagination-select">
+      <select :value="page" @change="selectChanged">
+        <option v-for="value in values" v-bind:value="value">{{value}}</option>
+      </select>
+      <button type="button" class="ui-button type-blank caret-down">
+        <span class="ui-button-text" v-localize="{ key: '@ui.pagination.xofy', tokens: { x: page, y: pages }}"></span>
+        <i class="ui-button-caret fth-chevron-down"></i>
+      </button>
+    </div>
+    <ui-icon-button class="ui-pagination-next" type="white" title="@ui.pagination.next" icon="fth-chevron-right" :disabled="page >= pages" @click="set(page + 1)" />
   </div>
 </template>
 
@@ -31,13 +30,11 @@
       }
     },
 
-    data: () => ({
-      
-    }),
-
-    mounted ()
-    {
-      
+    computed: {
+      values: function ()
+      {
+        return Array.apply(null, Array(this.pages)).map(function (_, i) { return i + 1; });
+      }
     },
 
     methods: {
@@ -46,6 +43,11 @@
       {
         this.$emit('update:page', page);
         this.$emit('change', page);
+      },
+
+      selectChanged(ev, a)
+      {
+        this.set(+ev.target.value);
       }
 
     }
@@ -63,6 +65,24 @@
 
   .ui-pagination-select
   {
-    margin: 0 20px;
+    margin: 0 30px;
+    position: relative;
+
+    .ui-button
+    {
+      padding: 0 2px;
+    }
+
+    select
+    {
+      width: 100%;
+      position: absolute;
+      z-index: 1;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      opacity: 0;
+    }
   }
 </style>
