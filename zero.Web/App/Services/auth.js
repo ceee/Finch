97d4a8aa3187768bd45b7b4db6e03b782cc3c697
Tobias Klika <cee@live.at>
2020-05-06 10:23:51 +0200

    fix login bug

--- zero.Web/App/Services/auth.js
+++ zero.Web/App/Services/auth.js
@@ -55,12 +55,13 @@ export default new Vue({
     {
       return Axios.post('authentication/loginUser', model).then(res =>
       {
+        console.info(res);
         return new Promise((resolve, reject) =>
         {
           if (res.data.success)
           {
             this.setUser(res.data.model);
-            resolve(res.data.model);
+            resolve(res.data);
           }
           else
           {
