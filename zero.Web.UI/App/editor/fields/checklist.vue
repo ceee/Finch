9019f9b05633e99012b3b60cb41f6c4b9ef2b0e1
Tobias Klika <cee@live.at>
2020-06-29 15:36:02 +0200

    add check-list

---
+++ zero.Web.UI/App/editor/fields/checklist.vue
@@ -0,0 +1,15 @@
+﻿<template>
+  <ui-check-list :value="value" @input="$emit('input', $event)" :reverse="config.reverse" :items="config.items" />
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        type: Array,
+        default: () => []
+      },
+      config: Object
+    },
+  }
+</script>
