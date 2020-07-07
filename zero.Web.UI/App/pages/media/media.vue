0a2dcda2b383fa3ff23166cbec2ef4c8850a76a3
Tobias Klika <cee@live.at>
2020-07-07 15:11:29 +0200

    update display of media items

--- zero.Web.UI/App/pages/media/media.vue
+++ zero.Web.UI/App/pages/media/media.vue
@@ -369,12 +369,13 @@
     color: var(--color-fg);
     font-size: var(--font-size-xs);
     position: relative;
+    padding: 10px;
 
     img
     {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
+      max-width: 100%;
+      max-height: 100%;
+      object-fit: contain;
     }
 
     &.is-blank
