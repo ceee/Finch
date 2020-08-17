61c6e3b0841f76e8c2f8a038f6a9c8beb78c0bc4
Tobias Klika <cee@live.at>
2020-08-17 10:59:13 +0200

    move date-range-picker into core

---
+++ zero.Web.UI/App/components/pickers/dateRangePicker/daterangepicker.vue
@@ -0,0 +1,130 @@
+﻿<template>
+  <div class="ui-daterangepicker" :class="{'is-disabled': disabled }">
+    <button type="button" class="ui-link" @click="schedule" v-localize="scheduleLocalize"></button>
+  </div>
+</template>
+
+
+<script>
+  import Strings from 'zero/services/strings';
+  import { extend as _extend } from 'underscore';
+  import DaterangepickerOverlay from './overlay';
+  import Overlay from 'zero/services/overlay.js';
+  import dayjs from 'dayjs';
+
+  const DATETIME_FORMAT = 'DD.MM.YY HH:mm';
+  const DATE_FORMAT = 'DD.MM.YY';
+
+  export default {
+    name: 'uiDaterangepicker',
+
+    components: { DaterangepickerOverlay },
+
+    props: {
+      value: {
+        type: Object,
+        default: {
+          from: null,
+          to: null
+        }
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      format: {
+        type: String,
+        default: null
+      },
+      time: {
+        type: Boolean,
+        default: false,
+      },
+      max: {
+        type: [String, Date],
+        default: null
+      },
+      min: {
+        type: [String, Date],
+        default: null
+      },
+      fromText: {
+        type: String,
+        default: '@page.schedule.publish'
+      },
+      toText: {
+        type: String,
+        default: '@page.schedule.unpublish'
+      },
+      amPm: {
+        type: Boolean,
+        default: false
+      },
+      options: {
+        type: Object,
+        default: () => { }
+      }
+    },
+
+
+    data: () => ({
+      id: null,
+      output: null,
+      pickerOptions: {}
+    }),
+
+
+    computed: {
+      scheduleLocalize()
+      {
+        return {
+          key: !this.value.from && !this.value.to ? '@ui.date.set' :
+            (this.value.from && !this.value.to ? '@ui.date.x' :
+              (!this.value.from && this.value.to ? '@ui.date.y' : '@ui.date.xtoy')),
+          tokens: {
+            x: Strings.date(this.value.from, this.format || (this.time ? DATETIME_FORMAT : DATE_FORMAT)),
+            y: Strings.date(this.value.to, this.format || (this.time ? DATETIME_FORMAT : DATE_FORMAT))
+          }
+        };
+      }
+    },
+
+
+    created()
+    {
+      this.id = 'daterangepicker-' + Strings.guid();
+    },
+
+
+    methods: {
+
+      schedule()
+      {
+        Overlay.open({
+          component: DaterangepickerOverlay,
+          options: {
+            format: this.format,
+            time: this.time,
+            max: this.max,
+            min: this.min,
+            fromText: this.fromText,
+            toText: this.toText,
+            amPm: this.amPm
+          },
+          model: {
+            from: this.value.from,
+            to: this.value.to
+          },
+        }).then(res =>
+        {
+          this.$emit('change', res);
+          this.$emit('input', res);
+        }, () => { });
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  
+</style>
--- zero.Web.UI/App/pages/pages/page.vue
+++ zero.Web.UI/App/pages/pages/page.vue
@@ -6,6 +6,8 @@
       </template>
     </ui-form-header>
 
+    <!-- <ui-daterangepicker :value="{ from: model.releaseDate, to: model.unreleaseDate }" @input="onRangeChange" /> -->
+
     <ui-editor v-if="!loading" :config="renderer" v-model="model" :meta="meta" :is-page="true" infos="none" :on-configure="onEditorConfigure" />
   </ui-form>
 </template>
