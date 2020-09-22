c4c2afa70699bd109e8f04f50a320787807291bd
Tobias Klika <cee@live.at>
2020-09-22 20:32:27 +0200

    remove obsolete projects

--- zero.TestData/TestFeatures.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace zero.TestData
-{
-  public static class TestFeatures
-  {
-    public const string Wishlist = "testdata.wishlist";
-
-    public const string SocialShopping = "testdata.socialshopping";
-  }
-}
