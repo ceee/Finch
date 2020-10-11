64762c33e2ba8790aa8930f093f4d1c360561676
Tobias Klika <cee@live.at>
2020-10-12 00:20:56 +0200

    first draft of page url provider

--- zero.Core/Routing/IPageUrlProvider.cs
+++ zero.Core/Routing/IPageUrlProvider.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using zero.Core.Api;
+using zero.Core.Entities;
 
 namespace zero.Core.Routing
 {
   public interface IPageUrlProvider
   {
+    /// <summary>
+    /// Get URL for a page
+    /// </summary>
+    UrlInfo GetUrl(ApplicationContext context, IPage page, IEnumerable<IPage> parents);
   }
 }
---
+++ zero.Core/Routing/PageUrlProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using zero.Core.Api;
+using zero.Core.Entities;
+
+namespace zero.Core.Routing
+{
+  public class PageUrlProvider : IPageUrlProvider
+  {
+    const char PATH_SEPARATOR = '/';
+
+    const bool TRAILING_SLASH = false;
+
+
+    /// <inheritdoc />
+    public UrlInfo GetUrl(ApplicationContext context, IPage page, IEnumerable<IPage> parents)
+    {
+      StringBuilder stringBuilder = new StringBuilder();
+
+      foreach(IPage parent in parents)
+      {
+        stringBuilder.Append(parent.Alias);
+        stringBuilder.Append(PATH_SEPARATOR);
+      }
+
+      stringBuilder.Append(page.Alias);
+      stringBuilder.Append(PATH_SEPARATOR);
+
+      if (!TRAILING_SLASH)
+      {
+        stringBuilder.Remove(stringBuilder.Length - 1, 1);
+      }
+
+      return new UrlInfo(stringBuilder.ToString(), true, "en-US");
+    }
+  }
+}
