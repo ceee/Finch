06502ba6005ebbf534a427584afefea52990ce86
Tobias Klika <cee@live.at>
2020-10-16 15:17:33 +0200

    start new renderer

---
+++ zero.Web.UI/App/core/editor.js
@@ -0,0 +1,104 @@
+﻿
+class Editor
+{
+  #alias;
+  #templateLabel = field => field;
+  #templateDescription = field => field;
+
+  tabs = [];
+
+
+  constructor(alias)
+  {
+    this.#alias = alias;
+  }
+
+
+  get alias()
+  {
+    return this.#alias;
+  }
+
+  /**
+   * Overrides the string generation for the label
+   * @param {function} callback - Called when a label is constructed
+   */
+  set onLabelCreate(callback)
+  {
+    if (typeof callback !== 'function')
+    {
+      console.warn(`[zero] onLabelCreate excepts a function as the first parameter`);
+      return;
+    }
+    this.#templateLabel = callback;
+  }
+
+  /**
+   * Overrides the string generation for the label description
+   * @param {function} callback - Called when a description is constructed
+   */
+  set onDescriptionCreate(callback)
+  {
+    if (typeof callback !== 'function')
+    {
+      console.warn(`[zero] onDescriptionCreate excepts a function as the first parameter`);
+      return;
+    }
+    this.#templateDescription = callback;
+  }
+
+
+  /**
+   * Add a new tab to the editor
+   * @param {string} alias - Alias for the tab
+   * @param {string} name - Name of the tab (can be a translation)
+   * @param {number|function} [count] - Output a count indicator
+   * @param {boolean|function} [disabled] - Conditionally disable the tab and its content
+   */
+  tab(alias, name, count, disabled)
+  {
+    if (typeof disabled !== 'undefined' && typeof disabled !== 'boolean' && typeof disabled !== 'function')
+    {
+      console.warn(`[zero] editor.tab: the disabled property has to be of type [boolean, function, undefined]`);
+      return;
+    }
+
+    if (typeof count !== 'undefined' && typeof count !== 'number' && typeof count !== 'function')
+    {
+      console.warn(`[zero] editor.tab: the count property has to be of type [number, function, undefined]`);
+      return;
+    }
+
+    if (this.tabs.find(x => x.alias === alias))
+    {
+      console.warn(`[zero] the editor ${this.#alias} does already have a tab with the alias ${alias}`);
+      return;
+    }
+
+    const tab = { alias, name, disabled, count };
+
+    this.tabs.push(tab);
+
+    return tab;
+  }
+
+
+  /**
+   * Add a new tab to the editor
+   * @param {string} path - Model path
+   * @param {object} renderer - Can either be a renderer object or a vue component
+   * @param {object} [options] - Custom options
+   * @param {string} [options.label] - A custom label for this field (otherwise it's generated via `onLabelCreate`)
+   * @param {string} [options.description] - A custom description for this field (otherwise it's generated via `onDescriptionCreate`)
+   * @param {string} [options.helpText] - Display a help text below the field
+   * @param {boolean|function} [options.condition] - Conditionally hide the field
+   * @param {boolean|function} [options.disabled=false] - Conditionally disable the field
+   * @param {string} [options.class] - Append HTML class to the generated property
+   */
+  field(path, renderer, options)
+  {
+    
+  }
+};
+
+export default Editor;
---
+++ zero.Web.UI/App/renderers/application-new.js
@@ -0,0 +1,30 @@
+﻿
+import Editor from '../core/editor.js';
+import ApplicationFeatures from '../pages/settings/application-features.vue';
+
+//import Text from 'text.vue';
+
+const editor = new Editor('application');
+
+editor.onLabelCreate = field => '@application.fields.' + field;
+editor.onDescriptionCreate = field => '@application.fields.' + field + '_text';
+
+editor.tab('general', '@ui.tab_general');
+const domains = editor.tab('domains', '@application.tab_domains', x => x.domains.length);
+const features = editor.tab('features', '@application.tab_features', x => x.features.length);
+
+editor.field('name', new Text(50), { label: '@ui.name' });
+editor.field('fullName', new Text(120));
+editor.field('email', new Text(120));
+editor.field('imageId', new Image());
+editor.field('iconId', new Image());
+
+domains.field('domains', new InputList({
+  limit: 10,
+  addLabel: '@application.fields.domains_add',
+  helpText: '@application.fields.domains_help'
+}));
+
+features.field('features', ApplicationFeatures);
+
+export default editor;
