37fe4777a611a8ab92c1d1ce601f4d9f77883bda
Tobias Klika <cee@live.at>
2020-10-20 00:38:22 +0200

    try it with powershell

---
+++ zero.Web.UI/zero.run.ps1
@@ -0,0 +1,14 @@
+﻿
+param(
+  # get, don't execute
+  [Parameter(Mandatory=$false)]
+  [Alias("g")]
+  [switch] $get = $false,
+
+  # execute a command
+  [Parameter(Mandatory=$false, ValueFromRemainingArguments=$true)]
+  [String[]]
+  $command
+)
+
+npx vue-cli-service serve
