4e85231eda963a19317097341d3f2ce7f81a8936
Tobias Klika <cee@live.at>
2020-10-30 16:13:43 +0100

    pages with url index

---
+++ zero.Web/ViewHelpers/ZeroPageHelper.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using zero.Core.Api;
+using zero.Core.Entities;
+using zero.Core.Routing;
+using zero.Core.Extensions;
+
+namespace zero.Web.ViewHelpers
+{
+  public class ZeroPageHelper : IZeroPageHelper
+  {
+    HttpContext HttpContext;
+
+    IMediaApi MediaApi;
+
+    /// <summary>
+    /// Media cache for repetitive queries within an HTTP request
+    /// </summary>
+    Dictionary<string, IMedia> Cache { get; set; } = new Dictionary<string, IMedia>();
+
+
+    public ZeroPageHelper(IHttpContextAccessor httpContextAccessor, IMediaApi mediaApi)
+    {
+      HttpContext = httpContextAccessor.HttpContext;
+      MediaApi = mediaApi;
+    }
+
+
+    /// <inheritdoc />
+    public async Task<IMedia> GetById(string id)
+    {
+      if (id.IsNullOrEmpty())
+      {
+        return null;
+      }
+
+      if (!Cache.TryGetValue(id, out IMedia media))
+      {
+        media = await MediaApi.GetById(id);
+        Cache.Add(id, media);
+      }
+
+      return media;
+    }
+
+
+    /// <inheritdoc />
+    public async Task<Dictionary<string, IMedia>> GetByIds(string[] ids)
+    {
+      HashSet<string> remoteIds = new HashSet<string>();
+      Dictionary<string, IMedia> items = new Dictionary<string, IMedia>();
+
+      foreach (string id in ids)
+      {
+        if (Cache.TryGetValue(id, out IMedia media))
+        {
+          items.Add(id, media);
+        }
+        else
+        {
+          remoteIds.Add(id);
+        }
+      }
+
+      if (remoteIds.Count > 0)
+      {
+        Dictionary<string, IMedia> remoteItems = await MediaApi.GetById(remoteIds);
+
+        foreach (var item in remoteItems)
+        {
+          items.Add(item.Key, item.Value);
+          Cache.Add(item.Key, item.Value);
+        }
+      }
+
+      return items;
+    }
+
+
+    /// <inheritdoc />
+    public async Task<string> GetUrl(string id, bool isAbsolute = false)
+    {
+      IMedia media = await GetById(id);
+      return media?.Source;
+    }
+
+
+    /// <inheritdoc />
+    public async Task<Dictionary<string, string>> GetUrls(string[] ids, bool isAbsolute = false)
+    {
+      Dictionary<string, IMedia> medias = await GetByIds(ids);
+      return medias.ToDictionary(x => x.Key, x => x.Value?.Source);
+    }
+  }
+
+
+  public interface IZeroPageHelper
+  {
+    /// <summary>
+    /// Get media by Id
+    /// </summary>
+    Task<IMedia> GetById(string id);
+
+    /// <summary>
+    /// Get media items by Ids
+    /// </summary>
+    Task<Dictionary<string, IMedia>> GetByIds(string[] ids);
+
+    /// <summary>
+    /// Get source for a media item
+    /// </summary>
+    Task<string> GetUrl(string id, bool isAbsolute = false);
+
+    /// <summary>
+    /// Get source for media items
+    /// </summary>
+    Task<Dictionary<string, string>> GetUrls(string[] ids, bool isAbsolute = false);
+  }
+}
