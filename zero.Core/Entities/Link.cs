29f01f4ee018b6e0f76173e6401de8254c4005d7
Tobias Klika <cee@live.at>
2020-12-01 16:01:10 +0100

    add link entity

---
+++ zero.Core/Entities/Link.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace zero.Core.Entities
+{
+  public class Link
+  {
+    public string ProviderAlias { get; set; }
+
+    public LinkTarget Target { get; set; }
+
+    public string UrlSuffix { get; set; }
+
+    public string Title { get; set; }
+
+    public Dictionary<string, string> Values { get; set; } = new Dictionary<string, string>();
+  }
+
+  public enum LinkTarget
+  {
+    Default = 0,
+    Self = 1,
+    Blank = 2
+  }
+}
