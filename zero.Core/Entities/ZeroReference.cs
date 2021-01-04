f88db58b314b2d6701b50cd1f09edb9593c61fda
Tobias Klika <cee@live.at>
2021-01-04 15:38:00 +0100

    create transform overload for zero reference

--- zero.Core/Entities/ZeroReference.cs
+++ zero.Core/Entities/ZeroReference.cs
@@ -1,4 +1,6 @@
-﻿namespace zero.Core.Entities
+﻿using System;
+
+namespace zero.Core.Entities
 {
   public class ZeroReference
   {
@@ -13,6 +15,15 @@
     public static ZeroReference From(IZeroEntity entity)
     {
       return entity == null ? null : new ZeroReference(entity);
+    }
+
+    public static ZeroReference From<T>(T entity, Func<T, string> transform) where T : IZeroEntity
+    {
+      return entity == null ? null : new ZeroReference()
+      {
+        Id = entity.Id,
+        Name = transform(entity)
+      };
     }
 
     public string Id { get; set; }
