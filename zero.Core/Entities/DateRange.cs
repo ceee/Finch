fd6c5447863d00d512fb4e7c154aa93bbec2d579
Tobias Klika <cee@live.at>
2021-08-18 12:39:01 +0200

    IsWithin() for DateRange

--- zero.Core/Entities/DateRange.cs
+++ zero.Core/Entities/DateRange.cs
@@ -7,5 +7,18 @@ namespace zero.Core.Entities
     public DateTimeOffset? From { get; set; }
 
     public DateTimeOffset? To { get; set; }
+
+    public bool IsWithin(DateTimeOffset date)
+    {
+      if (From.HasValue && date < From)
+      {
+        return false;
+      }
+      if (To.HasValue && date > To)
+      {
+        return false;
+      }
+      return true;
+    }
   }
 }
