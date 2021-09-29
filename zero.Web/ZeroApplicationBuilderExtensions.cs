f54cf2414c81b11442ab8d2b389aa1b0f59e8c59
Tobias Klika <cee@live.at>
2021-09-29 14:01:02 +0200

    updating builder

--- zero.Web/ZeroApplicationBuilderExtensions.cs
+++ zero.Web/ZeroApplicationBuilderExtensions.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using zero.Core.Extensions;
-using zero.Core.Options;
-using zero.Core.Routing;
 using zero.Web.Middlewares;
 
 namespace zero.Web
@@ -12,65 +7,12 @@ namespace zero.Web
   {
     public static IApplicationBuilder UseZero(this IApplicationBuilder app)
     {
-      IZeroOptions options = app.ApplicationServices.GetService<IZeroOptions>();
-
-      string path = options.BackofficePath.EnsureStartsWith('/').TrimEnd('/');
-
-      if (!options.Routing.ErrorReexecutionPath.IsNullOrEmpty())
-      {
-        app.UseStatusCodePagesWithReExecute(options.Routing.ErrorReexecutionPath.EnsureStartsWith('/'), "?statusCode={0}");
-      }
-
       app.UseMiddleware<ZeroContextMiddleware>();
-
       app.UseStaticFiles();
-
-      // map backoffice
-      app.UseWhen(ctx => ctx.Request.Path.ToString().StartsWith(path), builder =>
-      {
-        builder.UseRouting();
-
-        builder.UseAuthentication();
-        builder.UseAuthorization();
-
-        //builder.UseMiddleware<ZeroMiddleware>(options);
-
-        builder.UseEndpoints(endpoints =>
-        {
-          // setup route
-          //endpoints.MapControllerRoute(
-          //  name: "setup",
-          //  pattern: path + "/setup",
-          //  defaults: new
-          //  {
-          //    controller = "ZeroSetup",
-          //    action = "Index"
-          //  }
-          //);
-
-          //// routes for API
-          //endpoints.MapControllerRoute(
-          //  name: "api",
-          //  pattern: path + "/api/{controller}/{action}/{id?}"
-          //);
-
-          // fallbacks for SPA
-          endpoints.MapFallbackToController(path + "/{**path}", "Index", "ZeroBackoffice");
-        });
-      });
-
+      app.UseRouting();
+      app.UseAuthentication();
+      app.UseAuthorization();
       return app;
-    }
-
-
-    public static IApplicationBuilder UseZeroRoutes(this IApplicationBuilder app)
-    {
-      IZeroOptions options = app.ApplicationServices.GetService<IZeroOptions>();
-
-      return app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapDynamicControllerRoute<ZeroRoutesTransformer>("{**url}", state: null, order: 10);
-      });
     }
   }
 }
---
+++ zero.Web/ZeroEnpointRouteBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using zero.Core.Routing;
+
+namespace zero.Web
+{
+  public static class ZeroEndpointRouteBuilderExtensions
+  {
+    public static IEndpointRouteBuilder MapZeroBackoffice(this IEndpointRouteBuilder endpoints, string path = "/zero")
+    {
+      //IZeroOptions options = app.ApplicationServices.GetService<IZeroOptions>(); // TODO oO
+      // see https://our.umbraco.com/documentation/reference/routing/custom-routes#where-to-put-your-routing-logic
+      //string path = options.BackofficePath.EnsureStartsWith('/').TrimEnd('/');
+
+      endpoints.MapFallbackToController(path + "/{**path}", "Index", "ZeroBackoffice");
+      return endpoints;
+    }
+
+
+    public static IEndpointRouteBuilder MapZeroRoutes(this IEndpointRouteBuilder endpoints)
+    {
+      endpoints.MapDynamicControllerRoute<ZeroRoutesTransformer>("{**url}", state: null, order: 10);
+      return endpoints;
+    }
+  }
+}
