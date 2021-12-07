84235626f5cbaef709285c018d0071e0ca15fd03
Tobias Klika <cee@live.at>
2021-12-07 16:14:56 +0100

    editor canvas definitions

--- zero.Backoffice.UI/app/editor/types.ts
+++ zero.Backoffice.UI/app/editor/types.ts
@@ -5,32 +5,77 @@ export interface ZeroEditor
 }
 
 
+export class ZeroFieldComponent
+{
+
+}
+
+
+export class ZeroEditorCanvasFieldBase<TModel>
+{
+  field<TField>(path: string, component: ZeroFieldComponent): ZeroEditorField<TModel, TField>
+  {
+    return new ZeroEditorField();
+  }
+}
+
+
+export class ZeroEditorCanvasBase<TModel> extends ZeroEditorCanvasFieldBase<TModel>
+{
+  field<TField>(path: string, component: ZeroFieldComponent): ZeroEditorField<TModel, TField>
+  {
+    return new ZeroEditorField();
+  }
+
+  fieldset(): ZeroEditorCanvasFieldBase<TModel>
+  {
+    return new ZeroEditorCanvasFieldBase();
+  }
+}
+
+
+export class ZeroEditorCanvas<TModel> extends ZeroEditorCanvasBase<TModel>
+{
+  tab(alias: string, name: string): ZeroEditorTab<TModel>
+  {
+    return new ZeroEditorTab(alias, name);
+  }
+}
+
+
+export class ZeroEditorTab<TModel> extends ZeroEditorCanvasBase <TModel>
+{
+  alias: string;
+  name: string;
+
+  constructor(alias: string, name: string)
+  {
+    super();
+    this.alias = alias;
+    this.name = name;
+  }
+}
+
 
 export declare type BooleanExpression<TModel, TField> = (model: TModel, value: TField) => boolean;
 
 
-
-export interface ZeroEditorField<TModel, TField>
+export class ZeroEditorField<TModel, TField>
 {
-  when(condition: BooleanExpression<TModel, TField>): ZeroEditorField<TModel, TField>;
-
-  required(required: boolean): ZeroEditorField<TModel, TField>;
-
-  required(condition: BooleanExpression<TModel, TField>): ZeroEditorField<TModel, TField>;
-}
-
-
-var field = {
-  when(condition)
-  {
-    return this;
-  },
-  required(required: boolean)
-  {
-    return this;
-  },
-  required(condition: BooleanExpression<any, any>)
+  when(condition: BooleanExpression<TModel, TField>): ZeroEditorField<TModel, TField>
   {
     return this;
   }
-} as ZeroEditorField<any, any>;
+
+  required(required: boolean | BooleanExpression<TModel, TField>): ZeroEditorField<TModel, TField>
+  {
+    return this;
+  }
+}
+
+
+//var editor = new ZeroEditorCanvas<any>();
+
+//editor.field('hi', null);
+//editor.fieldset();
+//editor.tab('hi', 'ho').
