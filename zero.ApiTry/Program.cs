77a5c1a8ad47c6a9309910ee4e2bfb2c795aa1c5
Tobias Klika <cee@live.at>
2021-12-13 01:23:07 +0100

    ++

--- zero.ApiTry/Program.cs
+++ zero.ApiTry/Program.cs
@@ -17,7 +17,17 @@ app.MapWhen(ctx => ctx.Request.Path.StartsWithSegments("/zero/api"), branch =>
   branch.UsePathBase(new PathString("/zero/api"));
   branch.UseEndpoints(endpoints =>
   {
-    endpoints.MapControllers();
+    // TODO https://www.codeproject.com/Questions/5262381/How-to-add-prefix-to-URL-in-MVC-based-on-roles
+    // see 
+    /*
+     * routes.MapRoute(
+    name: "Default",
+    url: "{role}/{controller}/{action}/{id}",
+    defaults: new { role = "User", controller = "Home", action = "Index", id = UrlParameter.Optional }
+).RouteHandler = new UserRoleHandler();
+
+     */
+    endpoints.MapControllerRoute("name", "pattern").
   });
 });
 
