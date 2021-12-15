95ee08a66ebc6314fceff219e2b73c9ad831303f
Tobias Klika <cee@live.at>
2021-12-15 16:08:16 +0100

    blueprint status

---
+++ zero.Core/Architecture/Blueprints/BlueprintStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace zero.Architecture;
+
+public enum BlueprintStatus
+{
+  /// <summary>
+  /// This entity is standalone
+  /// </summary>
+  Standalone = 0,
+  /// <summary>
+  /// This entity is a blueprint
+  /// </summary>
+  Parent = 1,
+  /// <summary>
+  /// This entity is a child of blueprint
+  /// </summary>
+  Child = 2
+}
