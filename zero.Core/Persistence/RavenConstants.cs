68c6a92c33c4a90ed7372f1ff4cf803b0c2fc97d
Tobias Klika <cee@live.at>
2022-01-14 19:41:13 +0100

    helper for throttling indexes

---
+++ zero.Core/Persistence/RavenConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace zero.Persistence;
+
+public static partial class RavenConstants
+{
+  public static partial class Indexing
+  {
+    public const string ThrottlingTimeIntervalInMs = "Indexing.Throttling.TimeIntervalInMs";
+  }
+}
