380c6e22fdefd562f7388602c10071bd90c95923
Tobias Klika <cee@live.at>
2022-01-18 00:53:31 +0100

    Update user.vue

--- zero.Backoffice.UI/app/modules/users/user.vue
+++ zero.Backoffice.UI/app/modules/users/user.vue
@@ -1,5 +1,5 @@
 ﻿<template>
-  <ui-editor-page :api="api" editor="users:edit" title="@user.name" prefix="@user.list" route="users-edit" />
+  <ui-editor-page :api="api" editor="users:edit" title="@user.name" prefix="@user.users" route="users-edit" />
 </template>
 
 
