5f1425b43902cee7c1450098ad4ac2f4888e381c
Tobias Klika <cee@live.at>
2022-01-28 14:06:18 +0100

    potential fix for broken translation detail page (in production only)

--- zero.Backoffice.UI/app/modules/translations/schemas/editor.ts
+++ zero.Backoffice.UI/app/modules/translations/schemas/editor.ts
@@ -7,14 +7,14 @@ editor.resourcePrefix = '@translation.fields.';
 
 
 editor.field('key').text({ maxLength: 300 });
-editor.field('display', { readonly: true }).state({
+editor.field('display', { readonly: x => !x || x.id }).state({
   items: [
     { label: '@translation.display.text', value: 'text' },
     { label: '@translation.display.html', value: 'html' }
   ]
 });
 
-editor.field('value').setHidden(x => x.display === 'text').rte();
-editor.field('value').setHidden(x => x.display === 'html').textarea();
+editor.field('value', { hidden: x => !x || x.display === 'text' }).rte();
+editor.field('value', { hidden: x => !x || x.display === 'html' }).textarea();
 
 export default editor;
--- zero.Backoffice.UI/app/modules/translations/translation.vue
+++ zero.Backoffice.UI/app/modules/translations/translation.vue
@@ -1,7 +1,7 @@
 ﻿<template>
   <ui-editor-page :api="api" editor="translations:edit" title="@translation.name" prefix="@translation.list" route="translations-edit">
     <template v-slot:header="slotProps">
-      <ui-form-header v-model:value="slotProps.model" prefix="@translation.list" :title="slotProps.model.key || '@translation.name'" :force-title="true" :title-disabled="true" 
+      <ui-form-header v-if="slotProps" v-model:value="slotProps.model" prefix="@translation.list" :title="slotProps.model && (slotProps.model.key || '@translation.name')" :force-title="true" :title-disabled="true" 
                       :disabled="slotProps.readonly" :is-create="!slotProps.id" :state="slotProps.form.state" :can-delete="slotProps.canDelete" @delete="slotProps.onInternalDelete" />
     </template>
   </ui-editor-page>
